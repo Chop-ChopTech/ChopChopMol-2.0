140398b5da927662474191fd2789b755734044fc
Nguyen Pham <nguyenpham0806@gmail.com>
2025-03-28 15:22:37 -0700

    Fast loading

--- atom/atom.js
+++ atom/atom.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+
 export default class Atom {
     constructor(main, type, coordinates, id) {
         this.molecule = main.molecule;
@@ -9,20 +10,7 @@ export default class Atom {
         this.z = coordinates.z;
         this.type = type;
         this.id = id;
-        this.radius=this.molecule.atomSettings[this.type].radius
-        this.color=new THREE.Color(this.molecule.atomSettings[this.type].color)
-
-
-
-    }
-    drawAtom() {
-        
-        console.log();
-        const geometry = new THREE.SphereGeometry(this.radius,16,16);
-        const material = new THREE.MeshStandardMaterial({ color: this.color });
-
-        const cube = new THREE.Mesh(geometry, material);
-        cube.position.set(this.x, this.y, this.z);
-        this.main.scene.add(cube);
+        this.radius = this.molecule.atomSettings[this.type].radius;
+        this.color = new THREE.Color(this.molecule.atomSettings[this.type].color);
     }
 }
--- atom/molecule.js
+++ atom/molecule.js
@@ -1,30 +1,71 @@
 import * as THREE from 'three';
 import Atom from './atom.js';
+
 export default class Molecule {
-    constructor(main,atomSettings) {
+    constructor(main, atomSettings) {
         this.main = main;
         this.atoms = [];
         this.bonds = [];
         this.atomSettings = atomSettings;
+        this.instancedMesh = null;  // We will store the instanced mesh here
         console.log(this.atomSettings);
     }
-    init(data){
+
+    init(data) {
         console.log(data.numAtoms);
 
-        for (let i=0; i<data.numAtoms; i++) {
-            const x = data.atomData[i].x*4;
-            const y = data.atomData[i].y*4;
-            const z = data.atomData[i].z*4;
+        // Create the geometry and material for all atoms
+        const geometry = new THREE.SphereGeometry(1, 8, 8);  // Using a default radius of 1
+        const material = new THREE.MeshStandardMaterial({ vertexColors: true });
+
+        // Create an InstancedMesh with a placeholder for the number of instances
+        this.instancedMesh = new THREE.InstancedMesh(geometry, material, data.numAtoms);
+
+        // Create an InstancedBufferAttribute to store the colors for each atom
+        const colorAttribute = new THREE.InstancedBufferAttribute(new Float32Array(data.numAtoms * 3), 3);
+        this.instancedMesh.geometry.setAttribute('color', colorAttribute);
+
+        // Loop through the atom data and create each atom
+        // Loop through the atom data and create each atom
+        for (let i = 0; i < data.numAtoms; i++) {
+            const x = data.atomData[i].x * 4;
+            const y = data.atomData[i].y * 4;
+            const z = data.atomData[i].z * 4;
             const element = data.atomData[i].element;
             const coordinates = new THREE.Vector3(x, y, z);
-            const id =getRandomArbitrary(0, 1000);
-            this.atoms.push(new Atom(this.main, element, coordinates, id));
+            const id = getRandomArbitrary(0, 1000);
 
+            // Create each atom and update its position and color
+            const atom = new Atom(this.main, element, coordinates, id);
+            this.atoms.push(atom);
+
+            // Set the radius for this atom
+            const radius = this.atomSettings[element]?.radius || 1; // Default to 1 if no radius
+
+            // Set the instance matrix (position + scale) for this atom
+            const matrix = new THREE.Matrix4();
+            matrix.setPosition(atom.position);
+
+            // Apply scaling based on radius
+            matrix.scale(new THREE.Vector3(radius, radius, radius));
+
+            this.instancedMesh.setMatrixAt(i, matrix);
+
+            // Set the color for the atom based on the element type
+            const color = new THREE.Color(this.atomSettings[element].color);
+            colorAttribute.setXYZ(i, color.r, color.g, color.b);
         }
 
-        this.drawMolecule();
+        // Update after all atoms are added
+        this.instancedMesh.instanceMatrix.needsUpdate = true;
+        colorAttribute.needsUpdate = true;
+
+
+        // Add the instanced mesh to the scene
+        this.main.scene.add(this.instancedMesh);
     }
+
     drawMolecule() {
-        this.atoms.forEach(atom => atom.drawAtom());
+        // Optionally update instanced mesh properties each frame
     }
 }
