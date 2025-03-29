24bb672e0005613acfaac4410a6e626537dd086b
Nguyen Pham <nguyenpham0806@gmail.com>
2025-03-28 20:51:25 -0700

    Create Bonds

---
+++ atom/bond.js
@@ -0,0 +1,8 @@
+export default class Bond {
+    constructor(molecule,atom1, atom2, distance) {
+        this.molecule = molecule;
+        this.atom1 = atom1;
+        this.atom2 = atom2;
+        this.distance = distance;
+    }
+}
--- atom/molecule.js
+++ atom/molecule.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import Atom from './atom.js';
+import Bond from './bond.js';
 
 export default class Molecule {
     constructor(main, atomSettings) {
@@ -12,21 +13,20 @@ export default class Molecule {
     }
 
     init(data) {
-        console.log(data.numAtoms);
+        console.log(data);
+        // Data format:
+        // Example:
+        // 0: {element: 'O', x: 104.008, y: 103.223, z: 106.729}
+        // 
 
-        // Create the geometry and material for all atoms
-        const geometry = new THREE.SphereGeometry(1, 8, 8);  // Using a default radius of 1
+        const atomGeometry = new THREE.SphereGeometry(1, 8, 8);
         const material = new THREE.MeshStandardMaterial({ vertexColors: true });
 
-        // Create an InstancedMesh with a placeholder for the number of instances
-        this.instancedMesh = new THREE.InstancedMesh(geometry, material, data.numAtoms);
+        this.instancedMesh = new THREE.InstancedMesh(atomGeometry, material, data.numAtoms);
 
-        // Create an InstancedBufferAttribute to store the colors for each atom
         const colorAttribute = new THREE.InstancedBufferAttribute(new Float32Array(data.numAtoms * 3), 3);
         this.instancedMesh.geometry.setAttribute('color', colorAttribute);
 
-        // Loop through the atom data and create each atom
-        // Loop through the atom data and create each atom
         for (let i = 0; i < data.numAtoms; i++) {
             const x = data.atomData[i].x * 4;
             const y = data.atomData[i].y * 4;
@@ -35,23 +35,18 @@ export default class Molecule {
             const coordinates = new THREE.Vector3(x, y, z);
             const id = getRandomArbitrary(0, 1000);
 
-            // Create each atom and update its position and color
             const atom = new Atom(this.main, element, coordinates, id);
             this.atoms.push(atom);
 
-            // Set the radius for this atom
             const radius = this.atomSettings[element]?.radius || 1; // Default to 1 if no radius
 
-            // Set the instance matrix (position + scale) for this atom
             const matrix = new THREE.Matrix4();
             matrix.setPosition(atom.position);
 
-            // Apply scaling based on radius
             matrix.scale(new THREE.Vector3(radius, radius, radius));
 
             this.instancedMesh.setMatrixAt(i, matrix);
 
-            // Set the color for the atom based on the element type
             const color = new THREE.Color(this.atomSettings[element].color);
             colorAttribute.setXYZ(i, color.r, color.g, color.b);
         }
@@ -64,7 +59,34 @@ export default class Molecule {
         // Add the instanced mesh to the scene
         this.main.scene.add(this.instancedMesh);
         this.centerMolecule();
+        // this.createBonds(this.atoms, 30);
+        console.log(this.createBonds(this.atoms, 8));
+        console.log(this.atoms);
+        this.visualizeBonds(this.bonds);
+
     }
+    createBonds(atoms, threshold) {
+    
+        for (let i = 0; i < atoms.length; i++) {
+            for (let j = i + 1; j < atoms.length; j++) {
+                const atom1 = atoms[i];
+                const atom2 = atoms[j];
+                const atom1Position = atom1.position;
+                const atom2Position = atom2.position;
+                const distance = atom1Position.distanceTo(atom2Position);
+                console.log(distance);
+    
+                if (distance <= threshold) {
+                    // Create a bond between the two atoms
+                    const bond = new Bond(this, atom1, atom2, distance);
+                    this.bonds.push(bond);
+                }
+            }
+        }
+    
+        return this.bonds;
+    }
+    
     centerMolecule() {
         const boundingBox = new THREE.Box3().setFromObject(this.instancedMesh);
     
@@ -77,6 +99,18 @@ export default class Molecule {
     
         // Update the controls target to rotate around the center
     
+    }
+    visualizeBonds(bonds) {
+        bonds.forEach(bond => {
+            const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+            const geometry = new THREE.BufferGeometry();
+            const positions = new Float32Array(6); // Two points, each with x, y, z coordinates
+            positions.set([...bond.atom1.position.toArray(), ...bond.atom2.position.toArray()]);
+    
+            geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+            const line = new THREE.Line(geometry, material);
+            this.main.scene.add(line);
+        });
     }
     
     drawMolecule() {
