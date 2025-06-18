c930327c2ee603b8a080d6d471bc248cfde6160c
Nguyen Pham <nguyenpham0806@gmail.com>
2025-06-17 17:35:56 -0700

    Small changes

--- AI/main.py
+++ AI/main.py
@@ -11,6 +11,9 @@ client = OpenAI(
     api_key=os.getenv("XAI_API_KEY"),
     base_url="https://api.x.ai/v1",
 )
+# client = OpenAI(
+#     api_key=os.getenv("OPENAI_API_KEY"),
+# )
 
 
 @app.route("/chat", methods=["POST"])
@@ -19,10 +22,11 @@ def chat():
     try:
         response = client.chat.completions.create(
             model="grok-3-latest",
+            # model="gpt-4.1",
             messages=[
                 {
                     "role": "system",
-                    "content": 'You are a molecule generator. You generate the molecule the prompt asks with the greatest accuracy. You will generate a JSON file that contains the molecule data. Put it in this exact format: (DO NO CHANGE ANTYHING ABOUT THE FORMAT). Example 1: {"atomData": [{"element": "C", "x": 0.000, "y": 1.396, "z": 0.000},{"element": "H", "x": 1.209, "y": 0.698, "z": 0.000},{"element": "O", "x": 1.209, "y": -0.698, "z": 0.000}],"numAtoms": 3} Example 2: {"atomData": [{"element": "C", "x": 0.000, "y": 1.396, "z": 0.000},{"element": "C", "x": 1.309, "y": 0.498, "z": 0.200},{"element": "O", "x": 1.209, "y": -0.698, "z": 2.014},{"element": "N", "x": 0.209, "y": -0.633, "z": 1.020},{"element": "S", "x": 1.119, "y": -0.491, "z": 6.025}],"numAtoms": 5}  ONLY GENERATE THE MOLECULE JSON FILE IN THE GIVEN EXAMPLE FORMATS AND NOTHING ELSE',
+                    "content": 'You are the worlds greatest and most accurate molecule generator. You generate the molecule the prompt asks with the greatest accuracy. You will generate a JSON file that contains the molecule data. Put it in this exact format: (DO NO CHANGE ANTYHING ABOUT THE FORMAT). Example 1: {"atomData": [{"element": "C", "x": 0.000, "y": 1.396, "z": 0.000},{"element": "H", "x": 1.209, "y": 0.698, "z": 0.000},{"element": "O", "x": 1.209, "y": -0.698, "z": 0.000}],"numAtoms": 3} Example 2: {"atomData": [{"element": "C", "x": 0.000, "y": 1.396, "z": 0.000},{"element": "C", "x": 1.309, "y": 0.498, "z": 0.200},{"element": "O", "x": 1.209, "y": -0.698, "z": 2.014},{"element": "N", "x": 0.209, "y": -0.633, "z": 1.020},{"element": "S", "x": 1.119, "y": -0.491, "z": 6.025}],"numAtoms": 5} Bonds are created via the covelant radius system so keep that in mind. ONLY GENERATE THE MOLECULE JSON FILE IN THE GIVEN EXAMPLE FORMATS AND NOTHING ELSE',
                 },
                 {"role": "user", "content": user_message},
             ],
